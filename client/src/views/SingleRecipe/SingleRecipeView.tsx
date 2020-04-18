6af89421e5b31d92311abb0f3c37bb72fc263cf9
Tomi <tomi.turtiainen@gmail.com>
2020-04-18 17:16:35 +0300

    Add speaking of recipe

--- client/src/views/SingleRecipe/SingleRecipeView.tsx
+++ client/src/views/SingleRecipe/SingleRecipeView.tsx
@@ -14,10 +14,12 @@ import { useOvermind, Recipe, RecipeIngredient } from '../../overmind';
 import { NoRecipeFound } from './NoRecipeFound';
 import { useNavigation } from '../../navigation';
 import { formatQuantity, formatDuration } from '../../ui/formatters';
+import { speechAvailable, speakText, stopSpeaking } from './speechSynth';
 
 export interface SingleRecipeViewProps {}
 
 export const SingleRecipeView: React.FC<SingleRecipeViewProps> = () => {
+  const [isSpeaking, setIsSpeaking] = React.useState(false);
   const { recipeId } = useParams();
   const { actions, state } = useOvermind();
   const { navigateToAllRecipes } = useNavigation();
@@ -46,6 +48,35 @@ export const SingleRecipeView: React.FC<SingleRecipeViewProps> = () => {
     return <NoRecipeFound />;
   }
 
+  const speakIngredients = () => {
+    setIsSpeaking(!isSpeaking);
+
+    if (isSpeaking) {
+      stopSpeaking();
+      return;
+    }
+
+    const ingredientsText = recipe.ingredients
+      .map(
+        (i) =>
+          `${formatQuantity(i.quantity)} ${i.unit ?? ''} ${i.ingredientText}`,
+      )
+      .join('. ');
+
+    speakText(ingredientsText, 'fi-FI', () => setIsSpeaking(false));
+  };
+
+  const speakInstructions = () => {
+    setIsSpeaking(!isSpeaking);
+
+    if (isSpeaking) {
+      stopSpeaking();
+      return;
+    }
+
+    speakText(recipe.instructions, 'fi-FI', () => setIsSpeaking(false));
+  };
+
   return (
     <AppLayout>
       <AppLayout.Header>
@@ -57,17 +88,37 @@ export const SingleRecipeView: React.FC<SingleRecipeViewProps> = () => {
         </Heading>
         <div className="mt-4 flex flex-row">
           <Pane display="flex" flexDirection="column" className="p-4">
-            <Heading size={600}>Ingredients</Heading>
-            <Text size={300} className="mt-2">
-              {recipe.numPortions} portions
-            </Text>
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <Heading size={600}>Ingredients</Heading>
+                <Text size={300} className="mt-2">
+                  {recipe.numPortions} portions
+                </Text>
+              </div>
+              {speechAvailable && (
+                <IconButton
+                  onClick={speakIngredients}
+                  icon={isSpeaking ? 'stop' : 'play'}
+                />
+              )}
+            </div>
             <Ingredients recipe={recipe} />
           </Pane>
-          <Pane className="p-4">
-            <Heading size={600}>Instructions</Heading>
-            <Text size={300} className="mt-2">
-              {formatDuration(recipe.duration)}
-            </Text>
+          <Pane className="p-4 flex-1">
+            <div className="flex justify-between">
+              <div>
+                <Heading size={600}>Instructions</Heading>
+                <Text size={300} className="mt-2">
+                  {formatDuration(recipe.duration)}
+                </Text>
+              </div>
+              {speechAvailable && (
+                <IconButton
+                  onClick={speakInstructions}
+                  icon={isSpeaking ? 'stop' : 'play'}
+                />
+              )}
+            </div>
             <Instructions recipe={recipe} />
           </Pane>
         </div>
